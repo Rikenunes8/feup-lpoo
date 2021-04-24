3eec77ceba403b0e8fe7169720732c1a4abe8689
Henrique Nunes <hrn2001@gmail.com>
2021-04-24 13:55:08 +0100

    Refactoring

---
+++ Exercises/refactoring/src/main/java/com/aor/refactoring/example5/Turtle.java
@@ -0,0 +1,56 @@
+package com.aor.refactoring.example5;
+
+public class Turtle {
+    private int row;
+    private int column;
+    private char direction;
+
+    public Turtle(int row, int column, char direction) {
+        this.row = row;
+        this.column = column;
+        this.direction = direction;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public char getDirection() {
+        return direction;
+    }
+
+    public void execute(char command) {
+        if (command == 'L') {
+            rotateLeft();
+        } else if (command == 'R') {
+            rotateRight();
+        } else if (command == 'F') {
+            moveForward();
+        }
+    }
+
+    private void moveForward() {
+        if      (direction == 'N') row--;
+        else if (direction == 'S') row++;
+        else if (direction == 'W') column--;
+        else if (direction == 'E') column++;
+    }
+
+    private void rotateRight() {
+        if      (direction == 'N') direction = 'E';
+        else if (direction == 'E') direction = 'S';
+        else if (direction == 'S') direction = 'W';
+        else if (direction == 'W') direction = 'N';
+    }
+
+    private void rotateLeft() {
+        if      (direction == 'N') direction = 'W';
+        else if (direction == 'W') direction = 'S';
+        else if (direction == 'S') direction = 'E';
+        else if (direction == 'E') direction = 'N';
+    }
+}
